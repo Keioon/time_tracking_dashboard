bf08b62da741b03f76c1f6d59b3c0d47bda8de18
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2022-01-25 19:40:59 +0100

    Add Card, Profil, install react router, add images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './styles/App.scss';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Profil from './components/Profil';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="app">
-      Das
-    </div>
+    <Router>
+      <div className="app">
+        <Profil/>
+        <Card/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Profil.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profil = () => (
+  <div className="profilBox">
+    <div className="mainBox">
+      <img src="./images/image-jeremy.png" alt="face"></img>
+    </div>
+  </div>
+);
+
+export default Profil;
