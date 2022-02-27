8f73394b4ec5d114990766fa3ad4c908b676234d
Krystian Urbaniak <krystian.g.urbaniak@gmail.com>
2022-02-27 23:44:40 +0100

    bugs repair

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './styles/App.scss';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react';
 import Profil from './components/Profil';
 import Day from './components/Day';
 import Week from './components/Week';
 import Month from './components/Month';
 
 function App() {
+  const routeName = 'time_tracking_dashboard';
+
+
   return (
     <Router>
       <div className='app'>
-        <Profil/>
+        <Profil routeName={routeName} />
         <div className='cardContainer'>
           <Routes>
-            <Route path='*' element={<Day/>}/>
-            <Route path='/day' element={<Day/>}/>
-            <Route path='/week' element={<Week/>}/>
-            <Route path='/month' element={<Month/>}/>
+            <Route path={`${routeName}/*`} element={<Day/>}/>
+            <Route path={`${routeName}/day`} element={<Day/>}/>
+            <Route path={`${routeName}/week`} element={<Week/>}/>
+            <Route path={`${routeName}/month`} element={<Month/>}/>
           </Routes>
         </div>
       </div>
